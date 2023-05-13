d1ee9a8269b0a75f44ce51d6611920c6ffa0ce86
liamczy <czy2517810944@aliyun.com>
2023-05-13 22:59:10 +0800

    23.5.13

---
+++ src/com/nuist/string/StringMethodTest1.java
@@ -0,0 +1,22 @@
+package com.nuist.string;
+
+import org.testng.annotations.Test;
+
+/**
+ * @author shkstart
+ * @create 2023-05-13 22:38
+ */
+public class StringMethodTest1 {
+    @Test
+    public void test1(){
+        String s1 =  "hello";
+        String s2 = s1.replace('l','w');
+        System.out.println(s1);
+        System.out.println(s2);//hewwo
+
+        String s3 = s1.replace("ll","wwww");
+        System.out.println(s3);//hewwwwo
+
+        //正则表达式：一个有规则要求的字符串--用户名大小写，邮箱
+    }
+}
